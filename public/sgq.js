6b89905f404d7208183015c7aea736d4eba10037
孙国强 <master@pygmalion.click>
2016-10-16 22:54:23 +0900

    drag and drop text fiels and preview them

---
+++ public/sgq.js
@@ -0,0 +1,6 @@
+function toList(arrayLikeData) {
+	return [].slice.apply(arrayLikeData);
+}
+function id(someId) {
+	return document.getElementById(someId);
+}
--- public/upload.js
+++ public/upload.js
@@ -11,5 +11,31 @@ function initializeDropArea(dropArea) {
 	});
 	dropArea.addEventListener("dragleave", event => {
 		dropArea.classList.remove("dragover");
+	});
+	dropArea.addEventListener("drop", event => {
+		event.preventDefault();
+		dropArea.classList.remove("dragover");
+		processUploadedFiles(event.dataTransfer.files);
 	})
 }
+
+/**
+ * @param files: FileList
+ */
+function processUploadedFiles(fileList) {
+	var files = toList(fileList);
+	if (!confirm(`Are you sure to upload below files?\n\n ${files.map(f => f.name.trim()).join("\n")}`)) return;
+	var fileReaders = files.map(file => new Promise((resolve, reject) => {
+		var reader = new FileReader();
+		reader.onload = e => {
+			resolve({name: file.name, content: e.target.result});
+		};
+		reader.readAsText(file);
+	}));
+	Promise.all(fileReaders).then(contents => {
+		var displayStr = contents.map(f => f.name + "\n\n" + f.content)
+			.join("\n\n---------------------------------------------------------------------------------\n\n");
+		id("content").textContent = displayStr;
+	});
+}
+
