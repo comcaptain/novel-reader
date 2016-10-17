d8c0872d34d11aae07cb465657e2d0920fc23232
孙国强 <master@pygmalion.click>
2016-10-17 21:44:10 +0900

    only show number of characters per chapter

--- public/novel.js
+++ public/novel.js
@@ -34,35 +34,13 @@ function loadNovel() {
 		xhr.send();
 	})
 }
-var CHAPTER_REGEX = /[第（][一二三四五六七八九零十百千万]+[）章][^\n]*/g;
-function parseNovel(content) {
-	var firstLineTo = content.indexOf("\n");
-	var novelName = content.substring(0, firstLineTo).trim();
-	var secondLineTo = content.indexOf("\n", firstLineTo + 1);
-	var author = content.substring(firstLineTo + 1, secondLineTo).trim();
-	content = content.substring(secondLineTo);
-	var chapterNames = content.match(CHAPTER_REGEX);
-	var chapterTexts = content.split(CHAPTER_REGEX);
-	chapterTexts.splice(0, 1);
-	var chapters = [];
-	for (var i = 0, j = 0; i < chapterNames.length; i++) {
-		if (chapterTexts[i] == undefined || chapterTexts[i].length < 300) continue;
-		chapters[j++] = {name: chapterNames[i], content: chapterTexts[i].replace(/^[\s\n]*/m, "    ")};
-	}
-	return {
-		name: novelName,
-		author: author,
-		chapters: chapters
-	};
-}
 function renderChapter(novel, index) {
 	var template = document.getElementById("chapter-template");
 	var clone = document.importNode(template.content, true);
 	var chapter = novel.chapters[index];
 	clone.querySelector(".chapter-title").textContent = chapter.name;
 	clone.querySelector(".body").textContent = chapter.content;
-	clone.querySelector(".chapter-info").textContent = "小说:" + 
-		novel.name + "  作者:" + novel.author + "  字数:" + chapter.content.length;
+	clone.querySelector(".chapter-info").textContent = `字数:${chapter.content.length}`;
 	clone.children[0].id = generateChapterId(index);
 	return clone;
 }
