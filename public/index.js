57eb24575113b5f153112e3d3bd85c4537d44998
孙国强 <master@pygmalion.click>
2016-10-17 22:05:45 +0900

    show book list

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-	$.get("novelList", undefined, data => {
-		$("#content").text(JSON.stringify(data));
-	}, "json");
+	$.get("novelList", undefined, renderIndex, "json");
 });
+
+function renderIndex(data) {
+	data.map(dataUrl => {
+		var novelName = dataUrl.match(/.*\/(.*)\.json/)[1];
+		var a = document.createElement("a");
+		a.classList.add("novel");
+		a.href = "novel?path=" + dataUrl;
+		a.textContent = novelName;
+		return a;
+	}).forEach(a => {
+		id("content").appendChild(a);
+	});
+}
